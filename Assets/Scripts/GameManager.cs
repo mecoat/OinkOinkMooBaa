3312f8ea8386a3eba98b0d31f27f214002cc89c9
mecoat <66117150+mecoat@users.noreply.github.com>
2025-05-31 14:07:12 +0100

    basic states added

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,13 +1,51 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager Instance;
+
+    public GameState state;
+
+    public static event Action<GameState> OnGameStateChanged;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public void UpdateGameState(GameState newState)
+    {
+        state = newState;
+
+
+        switch (newState)
+        {
+            case GameState.Spawning:
+                SpawnAnimals();
+                break;
+            case GameState.Playing:
+                break;
+            case GameState.Success:
+                break;
+            default:
+                break;
+        }
+
+        OnGameStateChanged?.Invoke(newState);
+    }
+
+    private void SpawnAnimals()
+    {
+
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        UpdateGameState(GameState.Spawning);
     }
 
     // Update is called once per frame
@@ -15,4 +53,11 @@ public class GameManager : MonoBehaviour
     {
         
     }
+}
+
+public enum GameState
+{
+    Spawning,
+    Playing,
+    Success
 }
