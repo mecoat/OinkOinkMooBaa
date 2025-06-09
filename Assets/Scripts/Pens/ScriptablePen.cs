2cf50643aac5d471d5de7e8fac7f36faeddcb896
mecoat <66117150+mecoat@users.noreply.github.com>
2025-06-09 12:07:17 +0100

    Base Pens Created

---
+++ Assets/Scripts/Pens/ScriptablePen.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Pen", menuName = "Scriptable Pen")]
+public class ScriptablePen : ScriptableObject
+{
+    public Pen Pen;
+
+    public Animal Animal;
+
+    public BasePen penPrefab;
+
+    public AudioClip animalSound;
+
+}
+
+public enum Pen
+{
+    pig,
+    cow,
+    sheep,
+    duck,
+    horse,
+    hen
+}
