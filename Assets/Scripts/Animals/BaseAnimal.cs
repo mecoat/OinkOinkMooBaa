5a9f1ace746ec52dc556abed98b9d3362c792fc6
mecoat <66117150+mecoat@users.noreply.github.com>
2025-06-12 15:39:00 +0100

    sounds moved from scriptableAnimal to prefab scripts

--- Assets/Scripts/Animals/BaseAnimal.cs
+++ Assets/Scripts/Animals/BaseAnimal.cs
@@ -6,6 +6,17 @@ public class BaseAnimal : MonoBehaviour
 {
     public Tile homeTile;
 
+    [SerializeField]
+    private AudioClip animalSound;
+
+    //holder for the desired Audiosource on the object
+    private AudioSource sound;
+
+    private void Awake()
+    {
+        sound = GetComponent<AudioSource>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,4 +28,12 @@ public class BaseAnimal : MonoBehaviour
     {
         
     }
+
+    // According to https://docs.unity3d.com/2022.3/Documentation/Manual/CrossPlatformConsiderations.html should work on mobile too
+    private void OnMouseDown()
+    {
+        //sound.Play()
+    }
+
+
 }
