1a5538e3b0adb384ef6dcaf4712cb02372307e85
mecoat <66117150+mecoat@users.noreply.github.com>
2025-06-12 17:10:59 +0100

    moving and colliding, but collision not registring in oncollisionenter

--- Assets/Scripts/Animals/BaseAnimal.cs
+++ Assets/Scripts/Animals/BaseAnimal.cs
@@ -15,11 +15,17 @@ public class BaseAnimal : MonoBehaviour
     //holder for the desired Audiosource on the object
     private AudioSource sound;
 
-    //private bool isMoving;
+    private bool isMoving = false;
+
+    private Rigidbody2D rb;
+
+    private Vector2 offset;
 
     private void Awake()
     {
         sound = GetComponent<AudioSource>();
+
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Start is called before the first frame update
@@ -37,11 +43,13 @@ public class BaseAnimal : MonoBehaviour
     // According to https://docs.unity3d.com/2022.3/Documentation/Manual/CrossPlatformConsiderations.html should work on mobile too
     private void OnMouseDown()
     {
-        //isMoving = true;
+        isMoving = true;
 
         sound.clip = animalSound;
         sound.loop = true;
         sound.Play();
+
+        offset = GetMousePos() - (Vector2)transform.position;
     }
 
     private void OnMouseUp()
@@ -49,15 +57,23 @@ public class BaseAnimal : MonoBehaviour
         sound.Stop();
 
         transform.position = homeTile.transform.position;
+
+        isMoving = false;
     }
 
     private void OnMouseDrag()
     {
-        transform.position = GetMousePos();
+        rb.MovePosition (GetMousePos() - offset);
     }
 
     Vector2 GetMousePos()
     {
         return Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Hit");
+        Debug.Log(collision.gameObject.tag);
     }
 }
