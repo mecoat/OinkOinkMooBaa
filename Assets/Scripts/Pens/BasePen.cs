e9eef436ac4a72e1be78d54a67e1a65dd02fb6d4
mecoat <66117150+mecoat@users.noreply.github.com>
2025-06-12 12:36:43 +0100

    detecting hit on pens

--- Assets/Scripts/Pens/BasePen.cs
+++ Assets/Scripts/Pens/BasePen.cs
@@ -13,19 +13,12 @@ public class BasePen : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began)
-        {
-            //Ray ray = Camera.main.ScreenPointToRay(Input.touches[0].position);
-            //RaycastHit hit;
-            //if (Physics.Raycast(ray, out hit))
-            //{
-              //  if (hit.collider != null)
-                //{
-                    Debug.Log("hit Me");
-                //}
-            //}
-        }
+           
     }
 
-    
+    // According to https://docs.unity3d.com/2022.3/Documentation/Manual/CrossPlatformConsiderations.html should work on mobile too
+    private void OnMouseDown()
+    {
+        Debug.Log("hit me" );
+    }
 }
